a1fbe11f3b2a5ed85c328353d4397d7ee66f5f67
Orit Malki <orit@hero-apps.io>
2021-01-30 23:53:01 +0200

    solution - shapes and divider theming

--- app/src/main/java/com/workshop7/themesandstylesworkshop/ui/news/NewsFragment.kt
+++ app/src/main/java/com/workshop7/themesandstylesworkshop/ui/news/NewsFragment.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
 import com.workshop7.themesandstylesworkshop.R
 
@@ -25,6 +27,9 @@ class NewsFragment : Fragment() {
                 ViewModelProvider(this).get(NewsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_news, container, false)
         val recyclerview = root.findViewById<RecyclerView>(R.id.news_recyclerview)
+        //TODO STEP 2: create a custom divider drawable - or use provided news_divider.xml from res/drawable
+        //TODO STEP 3: add item decoration to the recyclerview of type DividerItemDecoration()
+        recyclerview.addItemDecoration(DividerItemDecoration(recyclerview.context, LinearLayout.VERTICAL))
         newsViewModel.newsData(findNavController().currentDestination?.id!!).observe(viewLifecycleOwner, Observer {
             if (recyclerview.adapter == null) {
                 newsAdapter = NewsAdapter(it)
